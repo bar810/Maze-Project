83e115c580879f233ec81fd44e399346fed84c48
bar brownshtein <bar brownshtein@DESKTOP-AHAUPHC>
2016-09-16 18:22:24 +0300

    first push

---
+++ maze3dProject/src/boot/Run.java
@@ -0,0 +1,27 @@
+package boot;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import model.MyModel;
+import presenter.Presenter;
+import view.MyView;
+
+
+/*TODO:
+ fresvetb
+ gg
+*/
+
+public class Run {
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter out = new PrintWriter(System.out);
+		MyView view = new MyView(in, out);
+		MyModel model = new MyModel(50);
+		Presenter presenter = new Presenter(model, view,50);
+		model.addObserver(presenter);
+		view.addObserver(presenter);
+		view.start();
+	}
+}
